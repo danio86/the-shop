fb470892e7e6232ddc03026f57626200f45c7d16
danio86 <d.harder@gmx.at>
2023-10-16 18:57:27 +0000

    add singup form and navbar and signup css files

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+axios.defaults.baseURL="https://the-shop2-0-d8de1f67b769.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+/* axios intercepters catches api-user-response/request infos  */
+// export const axiosReq = axios.create();
+// export const axiosRes = axios.create();
