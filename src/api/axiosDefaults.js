4dabcedb90ae2a0d2aaacc7c1dbc49c764d96389
danio86 <d.harder@gmx.at>
2023-10-17 18:33:39 +0000

    connected with backend correctly

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://the-shop2-0-d8de1f67b769.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
