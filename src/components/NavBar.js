8377e69bfb2be74cef507b1afaa126f8135b292b
danio86 <d.harder@gmx.at>
2023-10-19 06:13:06 +0000

    add useClickOutsideToggle hook for the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,13 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import axios from "axios";
 import Avatar from "./Avatar";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
 
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
   const handleSignOut = async () => {
     try {
       await axios.post("dj-rest-auth/logout/");
@@ -77,7 +79,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top" expanded={expanded}>
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -85,7 +87,8 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPropertyIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
